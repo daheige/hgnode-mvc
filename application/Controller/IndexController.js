06e860433e1a1af91764377d42750dace1619f2e
daheige <zhuwei313@hotmail.com>
2017-12-24 16:09:19 +0800

    路由demo

--- application/Controller/IndexController.js
+++ application/Controller/IndexController.js
@@ -7,5 +7,28 @@ module.exports = {
     },
     test: function(req, res, next) {
         return res.send('hi,this is a test!' + helper.logic('Test').test());
+    },
+    info: function(req, res, next) {
+        return res.json({
+            code: 200,
+            message: "ok",
+            data: {
+                id: 1,
+                name: "heige",
+                sex: 1
+            }
+        })
+    },
+    //{"servers":[{"serverName":"heige_vpn","serverIp":"127.0.0.1"},{"serverName":"test_vpn","serverIp":"127.0.0.1"}],"serversId":"test_team"}
+    addUser: function(req, res, next) {
+        return res.json({
+            code: 200,
+            message: "ok",
+            data: {
+                info: req.body.servers,
+                ip: req.ip
+            },
+            request_time: Math.floor((new Date()).getTime() / 1000)
+        })
     }
 }
--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,9 @@ module.exports = function(app) {
     });
     app.get('/index', helper.controller('Index', 'index'));
     app.get('/test', helper.controller('Index', 'test'));
+    app.get('/info', helper.controller('Index', 'info'));
+    app.get('/user', function(req, res, next) {
+        res.send('heige');
+    });
+    app.post('/add-user', helper.controller('Index', 'addUser'));
 }
